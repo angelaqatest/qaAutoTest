dffcba50fcddb9b25386886e0cdc54cd9666742a
angelaqatest <164548633@qq.com>
2019-05-13 14:47:22 +0800

    testng project

--- src/test/java/com/pactera/qaAutoTest/AppTest.java
+++ src/test/java/com/pactera/qaAutoTest/AppTest.java
@@ -1,38 +1,35 @@
 package com.pactera.qaAutoTest;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  @Test
+  public void mainTest() throws InterruptedException {
+//    throw new RuntimeException("Test not implemented");
+	System.out.println( "AppTest Hello World!" );
+	System.setProperty("webdriver.chrome.driver", "E:\\Test\\webDriver\\chromedriver.exe");
+    WebDriver webDriver = new ChromeDriver();
+    webDriver.get("https://www.baidu.com/");
+    webDriver.manage().window().setSize(new Dimension(550, 762));
+    webDriver.findElement(By.id("kw")).sendKeys("qa");
+    webDriver.findElement(By.id("kw")).sendKeys(Keys.ENTER);
+    Thread.sleep(5000);
+    
+    webDriver.findElement(By.linkText("QA_百度百科")).click();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    Thread.sleep(5000);
+    webDriver.quit();
+    System.out.println( "AppTest Bye!" );
+  }
 }
